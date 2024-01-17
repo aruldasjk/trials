96dfbed5f320b38afcf7bf8e787bb09a23cc7fc7
Aruldas JK <aruldasjayakrishnan@gmail.com>
2024-01-17 15:16:04 +0530

    New Project

---
+++ renderer.js
@@ -0,0 +1,14 @@
+// renderer.js
+
+// Example: Add event listeners to buttons
+document.getElementById("applicationsButton").addEventListener("click", function() {
+    alert("Clicked Applications Button");
+});
+
+document.getElementById("featuresButton").addEventListener("click", function() {
+    alert("Clicked Features Button");
+});
+
+document.getElementById("howToUseButton").addEventListener("click", function() {
+    alert("Clicked How to Use Button");
+});
